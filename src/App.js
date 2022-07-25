26a46cd155d84f596050640f22206d6041690ce8
yamathe <manuelsegmar@gmail.com>
2022-07-25 17:41:25 -0500

    ADD: App authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import './styles'
 import LoginPage from './pages/login-page';
+import { tokenKey } from './config';
+
+import React from 'react';
 // import GraciasPage from './pages/gracias-page';
 // import Gracias from './components/Gracias';
 function App() {
+  const [user, setUser] = React.useState(sessionStorage.getItem(tokenKey) || null)
+
   return (
     <>
-         <LoginPage/>
+      { !user? <LoginPage user={user} setUser={setUser}/> : <h1>Ingresaste</h1>}
     </>
   );
 }
