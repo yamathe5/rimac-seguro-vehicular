9cc37d2352e044099791ffc46b822160590264e8
yamathe <manuelsegmar@gmail.com>
2022-07-28 16:06:31 -0500

    ADD: Presentation styles & refactor

--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -3,12 +3,12 @@ import GirlOnPhonePhone from "../../assets/Girl-on-phone-phone.png"
 export default function Presentation (){
   return (
   <div className="main-presentation">
-    <img className="main-presentation__img" src={GirlOnPhonePhone} alt="Rimac Logo"/>
-
+    {/* <img className="main-presentation__img" src={GirlOnPhonePhone} alt="Rimac Logo"/> */}
+    <div className="main-presentation__img"  alt="Rimac Logo"></div>
     <div className="main-presentation-text">
-      <p>!Nuevo!</p>
-      <h1>Seguro vehicular Tracking</h1>
-      <p>Cuentanos donde le haras seguimiento a tu seguro</p>
+      <p className="text-xs">!Nuevo!</p>
+      <h1 className="headline-md">Seguro vehicular <span className="red-300">Tracking</span></h1>
+      <p className="text-md">Cuentanos donde le haras seguimiento a tu seguro</p>
     </div>
   </div>
   )
