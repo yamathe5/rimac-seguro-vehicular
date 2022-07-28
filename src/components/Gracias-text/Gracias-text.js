e499f59646a590af9c875e3e8935440b73722c6a
yamathe <manuelsegmar@gmail.com>
2022-07-28 16:08:11 -0500

    ADD: Gracias page  styles & refactor

--- src/components/Gracias-text/Gracias-text.js
+++ src/components/Gracias-text/Gracias-text.js
@@ -1,9 +1,10 @@
 export default function GraciasText(){
-  return (<div className="gracias--text__container">
-    <h2> <p>¡Te damos la bienvenida! </p>
+  return (<div className="gracias--text__container"> 
+  <p className="red-300 headline-md">¡Te damos la bienvenida! </p>
+    <h2 className="headline-md" >
         Cuenta con nosotros para proteger tu vehículo</h2>
-    <p>Enviaremos la confirmación de compra de tu Plan Vehícular Tracking a tu correo:</p>
-    <span>joel.sanchez@gmail.com </span>
+    <p className="text-lg font-300">Enviaremos la confirmación de compra de tu Plan Vehícular Tracking a tu correo:</p>
+    <span className="text-lg font-300">joel.sanchez@gmail.com </span>
 
     <button>COMO USAR MI SEGURO</button>
   </div>)
