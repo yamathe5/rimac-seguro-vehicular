928fcc063d0d5408ba3058489469aec967d85ad4
yamathe <manuelsegmar@gmail.com>
2022-07-28 16:06:17 -0500

    ADD: Price  styles & refactor

--- src/components/Price/Price.js
+++ src/components/Price/Price.js
@@ -1,12 +1,22 @@
 export default function Price ({price}){
   return(
     <div className="price">
-      <div>
-        <p>${price}.00</p>
-        <p>MENSUAL</p>
-      </div>
+      <div className="price--container">
+        <div>
+          <p className="text-sm font-700">MONTO</p>
+          <p className="headline-sm">${price}.00</p>
+          <p className="text-xs">MENSUAL</p>
+        </div>
+        <div className="price--container__plan">
+          <h4 className="headline-xxs">El precio incluye:</h4>
+          <p className="text-md"><span className="green-300">✔</span> Llanta de respuesto</p>
+          <p className="text-md"><span className="green-300">✔</span> Analisis de motor</p>
+          <p className="text-md"><span className="green-300">✔</span> Aros gratis</p>
+        </div>
 
-      <button>LO QUIERO</button>
+        <button className="button">LO QUIERO</button>
+      </div>
     </div>
+    
   )
 }
