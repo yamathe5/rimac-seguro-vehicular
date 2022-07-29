2d6ae14487079b441ca7bc436850004b452c2502
yamathe <manuelsegmar@gmail.com>
2022-07-28 23:16:03 -0500

    FIX: Refactor Sidebar component

--- src/components/Sidebar/Sidebar-item.js
+++ src/components/Sidebar/Sidebar-item.js
@@ -1,11 +1,20 @@
+import { useAuth } from "../../context/auth-context"
+
 export default function SidebarItem (){
+
+  const {logout} = useAuth()
+
+  function handleLogout(){
+    logout()
+  }
   return (
     <>
       <div className="sidebar--mobile">
-        <button className="rounded-button__red">	&#x3C;</button>
+        <button onClick={handleLogout} className="rounded-button__red">	&#x3C;</button>
         <p className="text-xs font-700" >PASO 2 DE 2</p>
         <div className="pruple-bar"></div>
       </div>
+      
       <div className="sidebar--desktop">
         <div>
           <button className="rounded-button" >1</button>
