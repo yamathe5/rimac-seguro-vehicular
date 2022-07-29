9cedac40990aacc8896bac75076c5934add2ebaa
yamathe <manuelsegmar@gmail.com>
2022-07-28 23:24:11 -0500

    ADD: session services DNI validation

--- src/services/sessions-services.js
+++ src/services/sessions-services.js
@@ -5,7 +5,7 @@ import apiFetch from "./api-fetch";
 export async function login(credentials){
   let user = false;
   const users = await apiFetch("users", {
-    // body: credentials
+    // body
   });
 
 
@@ -14,6 +14,26 @@ export async function login(credentials){
     && user.celular === credentials.celular
     && user.placa ===  credentials.placa)
   })
+
+  let userById = users.find((user)=>{
+    return (user.documento === credentials.documento)
+  })
+
+
+  // user by id -> only id matches user 
+  if(userById){
+    credentials = {
+      ...credentials,
+      documento: userById.documento,
+      nombre: userById.nombre
+    }
+    user = await apiFetch("users", {
+      body: credentials
+    });
+    
+    return user
+  }
+
 
   // if there is an user 
   if(user){
