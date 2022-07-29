4f28b398e196f10139b8ceed53d0b8348fe02706
yamathe <manuelsegmar@gmail.com>
2022-07-28 23:20:08 -0500

    FIX: Refactor App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import './App.css';
 import './styles'
 import LoginPage from './pages/login-page';
-import { tokenKey } from './config';
-
 import React from 'react';
-import ArmaTuPlan from './pages/arma-tu-plan';
-import GraciasPage from './pages/gracias-page';
 import { useAuth } from './context/auth-context';
 import AuthenticatedApp from './AuthenticatedApp';
-// import Gracias from './components/Gracias';
+
 function App() {
-  // const [user, setUser] = React.useState(JSON.parse(sessionStorage.getItem(tokenKey)) || null)
   const { user } = useAuth();
   return (
     <>
-     {/* <GraciasPage/> */}
       { !user? <LoginPage/> : <AuthenticatedApp/>}
     </>
   );
