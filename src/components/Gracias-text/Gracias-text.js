0cf995348482298198416cf2d7ebeab309784bc3
yamathe <manuelsegmar@gmail.com>
2022-07-28 23:16:57 -0500

    FIX: Refactor Gracias text   component

--- src/components/Gracias-text/Gracias-text.js
+++ src/components/Gracias-text/Gracias-text.js
@@ -2,8 +2,12 @@ import { useAuth } from "../../context/auth-context.js"
 
 export default function GraciasText(){
 
-  const {user} =  useAuth();
-  console.log(user)
+  const {user, logout} =  useAuth();
+
+  function handleLogout(){
+    logout()
+  }
+
   return (<div className="gracias--text__container"> 
   <p className="red-300 headline-md">¡Te damos la bienvenida! </p>
     <h2 className="headline-md" >
@@ -11,6 +15,6 @@ export default function GraciasText(){
     <p className="text-lg font-300">Enviaremos la confirmación de compra de tu Plan Vehícular Tracking a tu correo:</p>
     <span className="text-lg font-300">{user.correo}</span>
 
-    <button>COMO USAR MI SEGURO</button>
+    <button onClick={handleLogout}>COMO USAR MI SEGURO | LOGOUT</button>
   </div>)
 }
