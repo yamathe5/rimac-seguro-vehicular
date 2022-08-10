41aec786721282248052062085cc9a14857b9b3a
yamathe <manuelsegmar@gmail.com>
2022-08-10 00:17:37 -0500

    FIX: bug al perseverar data

--- src/services/sessions-services.js
+++ src/services/sessions-services.js
@@ -4,44 +4,42 @@ import apiFetch from "./api-fetch";
 
 export async function login(credentials){
   let user = false;
-  const users = await apiFetch("users", {
-    // body
-  });
+  const users = await apiFetch("users", {});
 
-
+  // user que coincide documento y nro de placa
   user = users.find((user)=>{
-    return (user.documento === credentials.documento
-    && user.celular === credentials.celular
-    && user.placa ===  credentials.placa)
+    return (user.documento === credentials.documento && user.placa ===  credentials.placa)
   })
 
+  // return si encuentra un usuario 
+  if(user){
+    return user
+  }
+
+  // user que coincide por documento - este user tendra un carro distinto
   let userById = users.find((user)=>{
     return (user.documento === credentials.documento)
   })
 
 
-  // user by id -> only id matches user 
+  // Le asignamos su informacion que esta en la API como su documento, nombre y correo
+  // La otra informacion como el carro modelo año sera nueva
+  // solo coincide el nro de documento excepto la placa
   if(userById){
     credentials = {
       ...credentials,
       documento: userById.documento,
-      nombre: userById.nombre
+      nombre: userById.nombre,
+      correo: userById.correo
     }
     user = await apiFetch("users", {
       body: credentials
     });
-    
     return user
   }
 
 
-  // if there is an user 
-  if(user){
-    sessionStorage.setItem(tokenKey, JSON.stringify(user)) // save user data on storage
-    return user
-  }
-  
-  // if there is not an user
+  // si ninguna validacion funciono entonces creamos un nuevo usuario
   user = await apiFetch("users", {
     body: credentials
   });
